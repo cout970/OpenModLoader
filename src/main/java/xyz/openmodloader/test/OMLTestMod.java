6124b49f2d9e2bc55ea3dec5d4dbfe190d06fa14
iLexiconn <me@ilexiconn.net>
2016-06-22 00:10:20 +0200

    Updated test mod

--- src/main/java/xyz/openmodloader/test/OMLTestMod.java
+++ src/main/java/xyz/openmodloader/test/OMLTestMod.java
@@ -1,5 +1,6 @@
 package xyz.openmodloader.test;
 
+import net.minecraft.init.Blocks;
 import xyz.openmodloader.event.Events;
 import xyz.openmodloader.modloader.IMod;
 
@@ -7,8 +8,18 @@ public class OMLTestMod implements IMod {
     @Override
     public void onEnable() {
         System.out.println("Loading test mod");
+
         Events.BLOCK_PLACE.register(event -> {
             System.out.println("Placed block: " + event.getBlockState() + " isRemote: " + event.getWorld().isRemote);
+            if (event.getBlockState().getBlock() == Blocks.GRASS) {
+                event.setBlockState(Blocks.DIRT.getDefaultState());
+            }
+        });
+        Events.BLOCK_DESTROY.register(event -> {
+            System.out.println("Destroyed block: " + event.getBlockState() + " isRemote: " + event.getWorld().isRemote);
+            if (event.getBlockState().getBlock() == Blocks.GRASS) {
+                event.setCanceled(true);
+            }
         });
     }
 }
